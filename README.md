4651bbe79d557aa6f4e87d007b9dc16ae8aecd40
jimh <james.l.heinecke@gmail.com>
2023-05-08 20:57:38 -0500

    link updates

---
+++ README.md
@@ -0,0 +1,37 @@
+
+  # Title
+  title<br>
+  [![License: Apache](https://img.shields.io/badge/License-Apache-yellow.svg)](https://opensource.org/licenses/Apache)
+
+  # Description 
+  desc
+
+  # Table of Contents<br>
+  [1. Installation](#installation)<br>
+  [2. Usage](#usage)<br>
+  [3. License](#license)<br>
+  [4. Contributing](#contriburing)<br>
+  [5. Tests](#tests)<br>
+  [6. Questions](#questions)<br>
+  
+  # Installation<br>
+  inst
+
+  # Usage<br>
+  usage
+
+  # License<br>
+  This application is covered by the Apache license. 
+
+  # Contributing<br>
+  contr
+
+  # Tests<br>
+  test
+
+  # Questions<br>
+  quest
+------------------------------------------------
+  GitHub: jlh--> https://github.com/jlh
+
+  If you have additional question contact me at jlh
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,36 +18,37 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  Title:
-  ${data.title} [![License: ${data.license}](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/${data.license})
+  # Title
+  ${data.title}<br>
+  [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})
 
-  Description: 
+  # Description 
   ${data.description}
 
-  Table of Contents:
-  [1. Installation](https://github.com/jlheinecke/challenge-9#installation)
-  2. Usage
-  3. License
-  4. Contributing
-  5. Tests
-  6. Questions
-
-  Installation:
+  # Table of Contents<br>
+  [1. Installation](#installation)<br>
+  [2. Usage](#usage)<br>
+  [3. License](#license)<br>
+  [4. Contributing](#contriburing)<br>
+  [5. Tests](#tests)<br>
+  [6. Questions](#questions)<br>
+  
+  # Installation<br>
   ${data.installation}
 
-  Usage:
+  # Usage<br>
   ${data.usage}
 
-  License:
+  # License<br>
   This application is covered by the ${data.license} license. 
 
-  Contributing:
+  # Contributing<br>
   ${data.contributing}
 
-  Tests:
+  # Tests<br>
   ${data.tests}
 
-  Questions:
+  # Questions<br>
   ${data.questions}
 ------------------------------------------------
   GitHub: ${data.git}--> https://github.com/${data.git}
